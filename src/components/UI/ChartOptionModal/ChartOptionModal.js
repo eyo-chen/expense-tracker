0421cfb4cb32064ebd363746d655681d3a6a19dd
OYE0303 <78901921+OYE0303@users.noreply.github.com>
2022-01-18 18:50:32 +0800

    responsive: chart section
    try to make chartOptionModal popup

---
+++ src/components/UI/ChartOptionModal/ChartOptionModal.js
@@ -0,0 +1,12 @@
+import ModalCloseIcon from "../ModalCloseIcon/ModalCloseIcon";
+import ChartOption from "../../MainContent/Chart/ChartOption/ChartOption";
+
+function ChartOptionModal(props) {
+  return (
+    <ModalCloseIcon>
+      <ChartOption />
+    </ModalCloseIcon>
+  );
+}
+
+export default ChartOptionModal;

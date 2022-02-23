84b6eede817ed233db02d34ca40cbacf312b6fcb
OYE0303 <78901921+OYE0303@users.noreply.github.com>
2022-02-24 01:03:53 +0800

    .

--- src/Others/CreateUserID/createUserID.js
+++ src/Others/CreateUserID/createUserID.js
@@ -2,7 +2,7 @@ import { auth } from "./../../firebase-config";
 
 function createUserID() {
   const user = auth.currentUser;
-  let userID = "dcwecwe";
+  let userID = "";
   if (user) {
     const { displayName, email } = user;
     userID = `${email}${displayName.split(" ").join("")}`;

afd18ac9265f465585fa5759d4c2ca29049816ec
CHEN YI YOU <78901921+OYE0303@users.noreply.github.com>
2024-03-30 18:34:35 +0800

    Feat/format delete model (#15)
    * feat: remove duplicate text
    
    * feat: remove id filed in delete model
    
    ---------
    
    Co-authored-by: Eyo Chen <eyo.chen@amazingtalker.com>

--- src/components/UI/DeleteModal/DeleteModal.js
+++ src/components/UI/DeleteModal/DeleteModal.js
@@ -16,8 +16,9 @@ function DeleteModal(props) {
   const { setFilteredData } = useContext(SearchListDataContext);
   const [, setEditModal] = useContext(EditModalContext);
   const [descriptionModal, setDescriptionModal] = useState(false);
-  const dataInfoKey = Object.keys(props.dataInfo);
-  const dataInfoValue = Object.values(props.dataInfo);
+  const {id: _, ...filteredDataInfo} = props.dataInfo // filter out the id
+  const dataInfoKey = Object.keys(filteredDataInfo);
+  const dataInfoValue = Object.values(filteredDataInfo);
 
   // first filter out the empty note
   const infoItem = dataInfoValue.filter((data, index) => {
@@ -131,7 +132,7 @@ function DeleteModal(props) {
 
         <p className={styles.description}>
           <em>
-            there is no way to get this data back once <br /> once you delete it
+            there is no way to get this data back <br /> once you delete it
           </em>
         </p>
         <div className={styles["btn__container"]}>

e96c4056a3ae9a3845ec19ebd9bf0e627d496f4c
Eyo Chen <eyo.chen@amazingtalker.com>
2024-03-30 18:19:13 +0800

    feat: get datalist at calendar

--- src/components/UI/ExpenseListModal/ExpenseListModal.js
+++ src/components/UI/ExpenseListModal/ExpenseListModal.js
@@ -1,34 +1,60 @@
+import {useState, useEffect} from "react";
 import style from "./ExpenseListModal.module.css";
 import ExpenseList from "../ExpenseList/ExpenseList";
 import SubTitle from "../SubTitle/SubTitle";
 import Button from "../Button/Button";
 import HorizontalLine from "../HorizontalLine/HorizontalLine";
 import Modal from "../Modal/Modal";
+import fetcher from "../../../Others/Fetcher/fetcher";
+import Loading from "../Loading/Loading";
 
 function ExpenseListModal(props) {
-  let scrollClassName = null;
-  if (props.expenseDataList.length > 6) scrollClassName = "scroll";
+  const [transactionList, setTransactionList] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   function addClickHandler() {
     props.expenseListModalToggler();
     props.addDataFormModalToggler();
   }
 
-  let mainContent =
-    props.expenseDataList.length === 0 ? (
+  useEffect(() => {
+    setLoading(true);
+    fetchTransactionList(props.selectedDate, props.selectedDate)
+      .then((data) => {
+        setTransactionList(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [props.selectedDate]);
+
+  let scrollClassName = null;
+  if (props.expenseDataList.length > 6) scrollClassName = "scroll";
+
+  let mainContent;
+  if (loading) {
+    mainContent = <Loading />;
+  } else if (transactionList.length === 0) {
+    mainContent = (
       <SubTitle className={`${style.empty} uppercase`}>no data</SubTitle>
-    ) : (
+    );
+  } else {
+    mainContent = (
       <ExpenseList
         classItem={
           scrollClassName
             ? `${style.list} ${style[scrollClassName]}`
             : `${style.list}`
         }
-        data={props.expenseDataList}
+        dataList={transactionList}
         modal={true}
         inDeleteSection={false}
       />
     );
+  }
 
   return (
     <Modal onClick={props.expenseListModalToggler} classModal={style.modal}>
@@ -58,3 +84,16 @@ function ExpenseListModal(props) {
 }
 
 export default ExpenseListModal;
+
+
+async function fetchTransactionList(startDate, endDate) {
+  try {
+    const res = await fetcher(
+      `v1/transaction?start_date=${startDate}&end_date=${endDate}`
+    );
+
+    return res.transactions
+  } catch (error) {
+    throw error;
+  }
+}

ef9e5f0c8446beeead97e17b56f77ae3da9fea60
Eyo Chen <eyo.chen@amazingtalker.com>
2024-04-28 20:10:27 +0800

    feat: setup line chart at account

--- src/components/MainContent/Account/AccountOverview/AccountChart.js
+++ src/components/MainContent/Account/AccountOverview/AccountChart.js
@@ -1,56 +1,72 @@
 import { useRef, useEffect, useState, useContext } from "react";
 import InputRadio from "../../../UI/InputRadio/InputRadio";
-import ExpenseDataContext from "../../../../store/expenseData/expenseData--context";
 import DisplayThemeContext from "../../../../store/displayTheme/displayTheme--context";
-import createLineDataArr from "../../../../Others/CreateChartData/createLineDataArr";
-import createLineLabels from "../../../../Others/CreateChartData/createLineLabels";
 import Chart from "chart.js/auto";
-import createConfigObj from "../../../../Others/CreateChartData/createConfigObj";
 import timeObj from "../../../../Others/TimeObj/timeObj";
 import styles from "./AccountChart.module.css";
-
-// for choosing diff time range (input radio)
-const timeRangeArr = ["1wk", "1mo", "3mo", "6mo", "1yr"];
+import formatDate from "../../../../Others/FormatDate/formatDate";
+import fetcher from "../../../../Others/Fetcher/fetcher";
+import Loading from "../../../UI/Loading/Loading";
 
 const { TODAY } = timeObj;
-
-// seven days ago
-const initialDate = new Date(new Date().setDate(TODAY.getDate() - 7));
-
-// an array of labels(string)
-const initialLabels = createLineLabels(initialDate, TODAY);
+const formattedEndDate = formatDate(TODAY);
 
 function AccountChart() {
-  const { expenseData } = useContext(ExpenseDataContext);
   const { displayTheme } = useContext(DisplayThemeContext);
-  const [chartData, setChartData] = useState([
-    initialLabels,
-    createLineDataArr(initialLabels, expenseData),
-  ]);
+  const [loading, setLoading] = useState(false);
+  const [date, setDate] = useState({
+    start: formatDate(new Date(new Date().setDate(TODAY.getDate() - 7))),
+    end: formattedEndDate,
+    timeRange: "one_week",
+  });
+  const [chartData, setChartData] = useState({
+    labels: [],
+    datasets: [],
+  });
   const chartRef = useRef(null);
 
-  const config = createConfigObj(...chartData, displayTheme, "account");
+  useEffect(() => {
+    setLoading(true);
+
+    fetchChartData(date.start, date.end, date.timeRange)
+      .then((data) => {
+        setChartData({
+          labels: data.labels,
+          datasets: data.datasets,
+        });
+      })
+      .catch((error) => console.error("Error fetching data:", error))
+      .finally(() => setLoading(false));
+  }, [date]);
 
   useEffect(() => {
+    if (loading) return;
+
+    const config = createLineChartConfig(chartData.labels, chartData.datasets, displayTheme);
     const chart = new Chart(chartRef.current, config);
 
     return function cleanUp() {
       chart.destroy();
     };
-  }, [config]);
+  }, [chartData, displayTheme, loading]);
 
   // helper function for changing time range
   function chartRadioChangeHandler(e) {
     const today = new Date();
     const value = e.target.value;
-    if (value === "1wk") today.setDate(today.getDate() - 7);
-    if (value === "1mo") today.setMonth(today.getMonth() - 1);
-    if (value === "3mo") today.setMonth(today.getMonth() - 3);
-    if (value === "6mo") today.setMonth(today.getMonth() - 6);
-    if (value === "1yr") today.setFullYear(today.getFullYear() - 1);
-    const labels = createLineLabels(today, TODAY);
-    const data = createLineDataArr(labels, expenseData);
-    setChartData([labels, data]);
+    if (value === "one_week") today.setDate(new Date().getDate() - 7);
+    if (value === "two_weeks") today.setDate(new Date().getDate() - 14);
+    if (value === "one_month") today.setMonth(new Date().getMonth() - 1);
+    if (value === "three_months") today.setMonth(new Date().getMonth() - 3);
+    if (value === "six_months") today.setMonth(new Date().getMonth() - 6);
+    if (value === "one_year") today.setFullYear(new Date().getFullYear() - 1);
+
+    setDate({ start: formatDate(today), end: formattedEndDate, timeRange: e.target.id });
+  }
+
+  let mainContent = <Loading className={styles.loading} />;
+  if (!loading) {
+    mainContent = <canvas className={styles.chart} ref={chartRef}></canvas>;
   }
 
   return (
@@ -59,12 +75,12 @@ function AccountChart() {
         onChange={chartRadioChangeHandler}
         className={styles["radio__container"]}
       >
-        {timeRangeArr.map((element, index) => (
+        {timeRangeArr.map(({label, value}, index) => (
           <InputRadio
-            key={element}
-            label={element}
-            id={element}
-            value={element}
+            key={value}
+            label={label}
+            id={value}
+            value={value}
             name="chart__btn"
             defaultChecked={index === 0}
             classLabel={`${styles.label} transition--25`}
@@ -73,44 +89,96 @@ function AccountChart() {
           />
         ))}
       </div>
-      <div className={styles["chart__container"]}>
-        <canvas className={styles.chart} ref={chartRef}></canvas>
-      </div>
+      <div className={styles["chart__container"]}>{mainContent}</div>
     </div>
   );
 }
 
 export default AccountChart;
-/*
-Reference 1
-const [chartData, setChartData] = useState([
-    initialLabels,
-    createLineDataArr(initialLabels, expenseData),
-  ]);
-the reason I choose to use array, instead of object
-is because this
-const config = createConfigObj(...chartData, displayTheme, "account");
-I can use ... to spread out the element, and pass in as argument
 
-Note
-Initially, i put line-config-object inside the this component
-but we later decide use line chart for net income
-So now it's not reasonable to have two exact same line-config-object in two diff components
-that's the one of main reason to seperate createChartDataArr and createConfigArr
-*/
 
-/*
-Reference 2
-createLineDataArr.bind(initialLabels, expenseData)
-vs.
-createLineDataArr(initialLabels, expenseData)
+async function fetchChartData(startDate, endDate, timeRange) {
+  try {
+    const data = await fetcher(`v1/transaction/line-chart?start_date=${startDate}&end_date=${endDate}&time_range=${timeRange}`, "GET");
 
-pass a function as initial value is better than passing the result of a function
-This make sure createLineDataArr will only run once in initial render
+    return data.chart_data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
 
-Try to use createLineDataArr(initialLabels, expenseData),
-and the function will run twice each time user click the button
-which is very unefficient
+// for choosing diff time range (input radio)
+const timeRangeArr = [
+  {
+    label: "1wk",
+    value: "one_week",
+  },
+  {
+    label: "2wk",
+    value: "two_weeks",
+  },
+  {
+    label: "1mo",
+    value: "one_month",
+  },
+  {
+    label: "3mo",
+    value: "three_months",
+  },
+  {
+    label: "6mo",
+    value: "six_months",
+  },
+  {
+    label: "1yr",
+    value: "one_year",
+  },
+];
 
-See detail in https://beta.reactjs.org/apis/usestate
-*/
+function createLineChartConfig(labels, data, displayTheme){
+  return {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          fill: {
+            target: "origin",
+          },
+          label: "",
+          data: data,
+          backgroundColor: ["rgba(54, 162, 235, 0.2)"],
+          borderColor: ["rgb(54, 162, 235)"],
+          borderWidth: 1,
+          pointBackgroundColor: ["rgb(54, 162, 235)"],
+        },
+      ],
+    },
+    options: {
+      maintainAspectRatio: false,
+      elements: {
+        line: {
+          tension: 0.5,
+        },
+      },
+      scales: {
+        y: {
+          beginAtZero: true,
+          ticks: {
+            color: `${
+              displayTheme === "dark" ? "rgb(210,210,210)" : "rgb(70,70,70)"
+            }`,
+          },
+        },
+        x: {
+          ticks: {
+            color: `${
+              displayTheme === "dark" ? "rgb(210,210,210)" : "rgb(70,70,70)"
+            }`,
+          },
+        },
+      },
+    },
+  };
+}

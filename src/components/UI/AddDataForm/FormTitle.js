2a9b2cd0756f6989ed294a0666cdc3df4aa374dc
Eyo Chen <eyo.chen@amazingtalker.com>
2024-05-04 20:01:26 +0800

    fix: correct adding data logic

--- src/components/UI/AddDataForm/FormTitle.js
+++ src/components/UI/AddDataForm/FormTitle.js
@@ -2,31 +2,63 @@ import InputRadio from "../InputRadio/InputRadio";
 import styles from "./AddDataForm.module.css";
 
 function FormTitle(props) {
+  const expenseTitle = (
+    <InputRadio
+      classInput={styles.input}
+      classCheck={`${styles.expense} ${styles.check}`}
+      classLabel={`${styles.title} uppercase transition--25`}
+      label="expense"
+      name="title"
+      value="expense"
+      id="expense"
+      onChange={props.categoryChangeHandler}
+      checked={props.type === "expense"}
+    />
+  );
+
+  const incomeTitle = (
+    <InputRadio
+      classInput={styles.input}
+      classCheck={`${styles.income} ${styles.check}`}
+      classLabel={`${styles.title} uppercase transition--25`}
+      label="income"
+      name="title"
+      value="income"
+      id="income"
+      onChange={props.categoryChangeHandler}
+      checked={props.type === "income"}
+    />
+  );
+
+  // when it's adding new data, we show both expense and income title
+  let titleContent = (
+    <>
+      {expenseTitle}
+      {incomeTitle}
+    </>
+  )
+
+  // when it's editing data, we show only the title of the data type
+  if (props.isEditing && props.type === "expense") {
+    titleContent = (
+      <>
+        {expenseTitle}
+      </>
+    )
+  }
+
+  // when it's editing data, we show only the title of the data type
+  if (props.isEditing && props.type === "income") {
+    titleContent = (
+      <>
+        {incomeTitle}
+      </>
+    )
+  }
+
+
   return (
-    <div className={styles["title__container"]}>
-      <InputRadio
-        classInput={styles.input}
-        classCheck={`${styles.expense} ${styles.check}`}
-        classLabel={`${styles.title} uppercase transition--25`}
-        label="expense"
-        name="title"
-        value="expense"
-        id="expense"
-        onChange={props.categoryChangeHandler}
-        checked={props.type === "expense"}
-      />
-      <InputRadio
-        classInput={styles.input}
-        classCheck={`${styles.income} ${styles.check}`}
-        classLabel={`${styles.title} uppercase transition--25`}
-        label="income"
-        name="title"
-        value="income"
-        id="income"
-        onChange={props.categoryChangeHandler}
-        checked={props.type === "income"}
-      />
-    </div>
+    <div className={styles["title__container"]}>{titleContent}</div>
   );
 }
 

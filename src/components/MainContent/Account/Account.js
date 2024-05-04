4e556e9d3e2642b7f4b7a2ba6578a6b916b32ef9
Eyo Chen <eyo.chen@amazingtalker.com>
2024-05-04 10:35:43 +0800

    refactor: remove news and small chart section

--- src/components/MainContent/Account/Account.js
+++ src/components/MainContent/Account/Account.js
@@ -1,12 +1,10 @@
 import AccountOverview from "./AccountOverview/AccountOverview";
-import AccountSubSection from "./AccountSubSection/AccountSubSection";
 import styles from "./Account.module.css";
 
 function Account() {
   return (
     <div className={styles.account}>
       <AccountOverview />
-      <AccountSubSection />
     </div>
   );
 }
--- src/components/MainContent/Account/AccountOverview/AccountOverview.js
+++ src/components/MainContent/Account/AccountOverview/AccountOverview.js
@@ -1,45 +1,24 @@
-import { useState } from "react";
 import Title from "../../../UI/Title/Title";
 import SubTitle from "../../../UI/SubTitle/SubTitle";
 import AccountCard from "./AccountCard";
 import AccountChart from "./AccountChart";
-import BtnIcons from "../../../UI/BtnIcons/BtnIcons";
-import AccountModal from "../../../UI/AccountModal/AccountModal";
 import { auth } from "../../../../firebase-config";
 import styles from "./AccountOverview.module.css";
 
 function AccountOverview() {
-  const [modalCard, setModalCard] = useState(false);
   const { displayName } = auth.currentUser;
 
-  function modalCardToggler(e) {
-    if (!modalCard) {
-      const id = e.target.dataset.id;
-      if (id) {
-        setModalCard(id);
-      }
-    } else setModalCard(false);
-  }
-
   return (
-    <>
-      {modalCard && (
-        <AccountModal modalCard={modalCard} closeModalCard={modalCardToggler} />
-      )}
-      <div className={styles.overview}>
-        <div className={styles.title}>
-          <Title>account overview</Title>
-          <SubTitle className={styles.subtitle}>
-            hi, {displayName ? displayName : "sample"}
-          </SubTitle>
-          <div className={styles["btn__container"]}>
-            <BtnIcons onClick={modalCardToggler} news={true} />
-          </div>
-        </div>
-        <AccountCard />
-        <AccountChart />
+    <div className={styles.overview}>
+      <div className={styles.title}>
+        <Title>account overview</Title>
+        <SubTitle className={styles.subtitle}>
+          hi, {displayName ? displayName : "sample"}
+        </SubTitle>
       </div>
-    </>
+      <AccountCard />
+      <AccountChart />
+    </div>
   );
 }
 

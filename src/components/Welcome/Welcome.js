f4828a1d98a78c56bd5a86c7bd493ae14dd149b1
CHEN YI YOU <78901921+OYE0303@users.noreply.github.com>
2024-05-12 16:38:26 +0800

    Feat/add welcome model (#40)
    * feat: add welcome model
    
    * feat: use welcome model
    
    * refactor: remove unused code
    
    ---------
    
    Co-authored-by: Eyo Chen <eyo.chen@amazingtalker.com>

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,31 @@
+import Title from "./../UI/Title/Title";
+import Modal from "./../UI/Modal/Modal";
+import Button from "./../UI/Button/Button";
+import { SiCashapp } from "react-icons/si";
+import styles from "./WelcomeModel.module.css";
+
+function Welcome() {
+  return (
+    <Modal classModal={`${styles.modal} center--flex`}>
+      <div>
+        <Title className={styles.title}>welcome</Title>
+        <Title className={styles.title}>to</Title>
+        <Title className={`${styles.title} ${styles["title__last"]}`}>
+          expen
+          <SiCashapp className={styles["title__icon"]} />e tracker
+        </Title>
+      </div>
+
+      <div className={styles["btn--container"]}>
+        <Button className={styles.btn}>
+          Login
+        </Button>
+        <Button className={styles.btn}>
+          Signup
+        </Button>
+      </div>
+    </Modal>
+  );
+}
+
+export default Welcome;

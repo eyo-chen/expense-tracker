22e691bfddf851f503dbad0d82f3dab6ab363078
Eyo Chen <eyo.chen@amazingtalker.com>
2024-05-18 09:50:20 +0800

    feat: use loginc component

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import Welcom from "./Welcom/Welcome";
 import Signup from "./Signup/Signup";
+import Login from "./Login/Login";
 
 function Auth() {
   const [authState, setAuthState] = useState("welcome");
 
   const KEY_TO_COMPONENT = {
     welcome: <Welcom setAuthState={setAuthState} />,
-    signup : <Signup setAuthState={setAuthState} />
+    signup : <Signup setAuthState={setAuthState} />,
+    login  : <Login setAuthState={setAuthState} />
   };
 
   return KEY_TO_COMPONENT[authState];

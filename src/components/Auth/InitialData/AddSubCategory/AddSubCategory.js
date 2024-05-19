4293fe39840adbf9c071545a5e5ca9352972736c
CHEN YI YOU <78901921+OYE0303@users.noreply.github.com>
2024-05-19 20:00:32 +0800

    Feat/add new init sub category data (#47)
    * feat: add sub category modal
    
    * feat: use sub category modal
    
    * fix: layout issue
    
    ---------
    
    Co-authored-by: Eyo Chen <eyo.chen@amazingtalker.com>

---
+++ src/components/Auth/InitialData/AddSubCategory/AddSubCategory.js
@@ -0,0 +1,115 @@
+import { useReducer } from "react";
+import Modal from "../../../UI/Modal/Modal";
+import Title from "../../../UI/Title/Title";
+import SubTitle from "../../../UI/SubTitle/SubTitle";
+import HorizontalLine from "../../../UI/HorizontalLine/HorizontalLine";
+import InputText from "../../../UI/InputText/InputText";
+import Button from "../../../UI/Button/Button";
+import Warning from "../../../UI/Warning/Warning";
+import styles from "./AddSubCategory.module.css"
+
+function AddingSubCategoryModal(props) {
+  const [form, formDispatch] = useReducer(reducer, {
+    name: "",
+    isDuplicate: false,
+    isTouch: false,
+    isValid: false,
+    categoryNameList: props.categoryNameList,
+  });
+
+  function inputTextTouchHandler() {
+    formDispatch({ type: "TOUCH" });
+  }
+
+  function inputTextChangeHandler(e) {
+    formDispatch({ type: "NAME", value: e.target.value });
+  }
+
+  function submitHandler(e) {
+    e.preventDefault();
+
+    props.addSubCategoryHandler(form.name);
+    props.addSubCategoryModalToggler();
+  }
+
+  const warnningIndex = form.isDuplicate || (form.isTouch && !form.isValid);
+
+  const warnningText = form.isDuplicate
+    ? "duplicate category name is not allowed"
+    : "required";
+
+  return (
+    <Modal onClick={props.addSubCategoryModalToggler} classModal={styles.modal}>
+      <Title className={styles.title}>add sub category</Title>
+      <HorizontalLine />
+      <div className={styles["subtitle__container"]}>
+        <SubTitle>you're adding sub category of {props.curMainCategory?.name}</SubTitle>
+      </div>
+      <form onSubmit={submitHandler}>
+        <div className={styles.container}>
+          <div className={styles["input__container"]}>
+            <InputText
+              name="subcategory name"
+              label="subcategory name"
+              value={form.name}
+              onChange={inputTextChangeHandler}
+              onBlur={inputTextTouchHandler}
+              classLabel={styles.label}
+              classInput={
+                warnningIndex
+                  ? `${styles.input} input--invalid`
+                  : `${styles.input}`
+              }
+            />
+          </div>
+
+          <Warning className={styles.warning} index={warnningIndex}>
+            {warnningText}
+          </Warning>
+        </div>
+
+        <div className={styles["btn__container"]}>
+          <Button
+            type="button"
+            className={`${styles.btn} uppercase btn--valid transition--25`}
+            onClick={props.addSubCategoryModalToggler}
+          >
+            cancel
+          </Button>
+          <Button
+            disabled={!form.isValid}
+            type="submit"
+            className={`${styles.btn} uppercase transition--25 ${
+              form.isValid ? `btn--valid` : "btn--invalid"
+            }`}
+          >
+            add
+          </Button>
+        </div>
+      </form>
+    </Modal>
+  );
+}
+
+export default AddingSubCategoryModal;
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "NAME": {
+      const isDuplicate = state.categoryNameList?.find(
+        name => name === action.value
+      );
+      const isValid = action.value.trim().length > 0 && !isDuplicate;
+
+      return { ...state, name: action.value, isValid, isDuplicate };
+    }
+
+    case "TOUCH": {
+      return { ...state, isTouch: true };
+    }
+
+    default: {
+      return state;
+    }
+  }
+}

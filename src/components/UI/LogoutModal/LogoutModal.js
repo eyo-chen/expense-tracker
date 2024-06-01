05fbd31e49d488d5fdd0503033c1e8f73aef6f8a
Eyo Chen <eyo.chen@amazingtalker.com>
2024-06-01 10:42:56 +0800

    feat: logout

--- src/components/UI/LogoutModal/LogoutModal.js
+++ src/components/UI/LogoutModal/LogoutModal.js
@@ -1,19 +1,17 @@
+import { useContext } from "react";
+import userInfoContext from "./../../../store/userInfo/userInfo--context";
 import Modal from "../Modal/Modal";
-import createUserID from "../../../Others/CreateUserID/createUserID";
 import HorizontalLine from "../HorizontalLine/HorizontalLine";
 import SubTitle from "../SubTitle/SubTitle";
 import Button from "../Button/Button";
-import { auth } from "../../../firebase-config";
-import { signOut } from "firebase/auth";
 import styles from "./LogoutModal.module.css";
 
 function LogoutModal(props) {
-  const [user] = createUserID();
-  const { photoURL, displayName, email } = user;
+  const { userInfo, setUserInfo } = useContext(userInfoContext);
 
   function signedOut() {
-    signOut(auth);
-    props.logoutModalToggler();
+    localStorage.removeItem("etoken");
+    setUserInfo(null);
   }
 
   return (
@@ -22,22 +20,11 @@ function LogoutModal(props) {
       <HorizontalLine />
       <SubTitle className={styles.subtitle}>current account</SubTitle>
       <div className={styles.info}>
-        {photoURL ? (
-          <>
-            <p className={styles.name}>{displayName}</p>
-            <img alt="user account" className={styles.img} src={photoURL} />
-          </>
-        ) : (
-          <>
-            <p className={styles.name}>sample</p>
-            <div className={`${styles.sample} center--flex`}>S</div>
-          </>
-        )}
-
-        <p className={styles.email}>{email}</p>
+        <p className={styles.name}>{userInfo?.name}</p>
+        <p className={styles.email}>{userInfo?.email}</p>
       </div>
       <div className={styles["btn__container"]}>
-        <Button onClick={props.logoutModalToggler} className={styles.btn}>
+        <Button onClick={props.logoutModalToggler} className={`${styles.btn} ${styles["btn--cancel"]}`}>
           cancel
         </Button>
         <Button onClick={signedOut} className={styles.btn}>

3639be092413b2b476b79e7d7fbaf26af5ee5f4a
Eyo Chen <eyo.chen@amazingtalker.com>
2024-10-27 14:13:30 +0800

    feat: change default theme to dark

--- src/App.js
+++ src/App.js
@@ -31,12 +31,12 @@ function App() {
   useEffect(() => {
     const theme = localStorage.getItem("displayTheme");
 
-    if (theme === "dark") {
-      document.body.classList.remove("light");
-      document.body.classList.add("dark");
-    } else {
+    if (theme === "light") {
       document.body.classList.remove("dark");
       document.body.classList.add("light");
+    } else {
+      document.body.classList.remove("light");
+      document.body.classList.add("dark");
     }
 
     setDisplayTheme(theme);
